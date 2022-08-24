a9fd3a9698c3a7b3babe2f5b5d83931bea9d1728
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-08-25 02:43:30 +1000

    refactor: change to cli

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Github PR File Changes Scrapper
 A web scrapper written in JS that retrieves file changes made in a given PR
 
+## How to use
+- Clone this repository
+- Go inside the repository
+- Run `npm install -g .`
+- Run `pcs -u url`. For example: `pcs -u https://github.com/org-name/repo-name/pull/1`
+- Your changed files will be printed
+
 ***Note: I made this purely because I'm too lazy to manually copy & paste file changes for my course's project PR***
+
--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-import * as readline from 'readline';
-import { stdin as input, stdout as output } from 'node:process';
+#!/usr/bin/env node
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
-const rl = readline.createInterface({ input, output });
-const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-
 try {
-    const prUrl = await prompt("Enter PR URL (example: https://github.com/org-name/repo-name/pull/1):");
-    const response = await fetch(`${prUrl}/files`);
+    const options = yargs(hideBin(process.argv))
+    .usage("Usage: -u <url>")
+    .option("u", {alias: "u", describe: "Gihub pull request URL", type: "string", demandOption: true})
+    .argv;
+    const response = await fetch(`${options.$0}/files`);
     const body = await response.text();
-
     const $ = cheerio.load(body);
-
     const fileNamesChanged = $('li.ActionList-item.js-tree-node > span').toArray().map((el) =>`- ${$(el).text()}` );
     const joinedFileNames = fileNamesChanged.join("\n");
     console.log(`\n${joinedFileNames}\n`);
-
 } catch (error) {
     console.log(error)
-} finally {
-    rl.close()
-}
+} 
